cda5308a7b79471868b4496ca7ba11a020771c5a
Diogo Vieira <diogoscsv@gmail.com>
2023-03-09 19:04:33 +0000

    initial commit

---
+++ file_intro_plugin/plugin.py
@@ -0,0 +1,38 @@
+from rigel.loggers import get_logger
+from rigel.plugins import Plugin as PluginBase
+from rigel.models.application import Application
+from rigel.models.builder import ModelBuilder
+from rigel.models.plugin import PluginRawData
+from rigel.models.rigelfile import RigelfileGlobalData
+from typing import Any, Dict
+from .models import PluginModel
+
+
+LOGGER = get_logger()
+
+class FileIntrospectionPlugin(PluginBase):
+
+    def __init__(
+        self,
+        raw_data: PluginRawData,
+        global_data: RigelfileGlobalData,
+        application: Application,
+        providers_data: Dict[str, Any]
+    ) -> None:
+        super().__init__(
+            raw_data,
+            global_data,
+            application,
+            providers_data
+        )
+
+        # self.model = ModelBuilder(PluginModel).build([], self.raw_data)
+
+    def setup(self) -> None:
+        pass
+
+    def run(self) -> None:
+        pass
+
+    def stop(self) -> None:
+        pass
