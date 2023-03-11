48a22ac0341df4b1135e81e891bee752795cb1fc
Diogo Vieira <diogoscsv@gmail.com>
2023-03-11 15:28:12 +0000

    now evaluates the level of quality of the results

--- file_intro_plugin/plugin.py
+++ file_intro_plugin/plugin.py
@@ -33,8 +33,34 @@ class FileIntrospectionPlugin(PluginBase):
         LOGGER.info("Running Setup")
 
     def run(self) -> None:
-        df = pandas.read_csv(self.model.files[0])
-        LOGGER.info(f"OUTPUT:\n {df}")
+
+        # Read csv results file
+        results_df = pandas.read_csv(self.model.files[0], index_col="Collection #")
+        LOGGER.info(f"OUTPUT:\n {results_df}")
+        
+        # Get the target value, which is the value used to evaluate the results
+        average_rms = results_df.loc["Averages", "RMS (pix)"]
+
+        LOGGER.info(f"Average RMS:\n {average_rms}")
+
+        # Value thresholds for the various levels of quality
+        acceptable_threshold = 3
+        good_threshold = 1
+        excellent_threshold = 0.5
+
+        # Quality levels
+        if average_rms <= 3:
+            results_quality = "acceptable"
+        else:
+            results_quality = "bad"
+
+        if average_rms <= 1:
+            results_quality = "good"
+
+        if average_rms <= 0.5:
+            results_quality = "great"
+
+        LOGGER.info(f"The results were {results_quality}!")
 
     def stop(self) -> None:
         LOGGER.info("Running Stop")
