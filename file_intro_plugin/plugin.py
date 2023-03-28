613068579ed99a7dec47a502fe22e6f6a90f8626
Kazadhum <diogoscsv@gmail.com>
2023-03-28 09:53:06 +0100

    done! #3

--- file_intro_plugin/plugin.py
+++ file_intro_plugin/plugin.py
@@ -106,7 +106,7 @@ class FileIntrospectionPlugin(PluginBase):
                     LOGGER.warning(f"'acceptable_max' is not an integer or float! Check your Rigelfile!")
                     acceptable_max = None
                 else:
-                    acceptable_min = column[column_name]['acceptable_max']
+                    acceptable_max = column[column_name]['acceptable_max']
 
                 if (column_keys.count('contains_str') < 1):
                     contains_str = None
@@ -135,6 +135,9 @@ class FileIntrospectionPlugin(PluginBase):
                     LOGGER.error(f"You can't choose to do introspection for both numeric and non-numeric values simultaneously. Only use the 'acceptable_min' and 'acceptable_max' fields for numeric values; only use the 'contains_str' and/or 'does_not_contain_str' fields for non-numeric values.")
                     exit()
                 
+                if (acceptable_max == None and acceptable_min == None and contains_str == None and does_not_contain_str == None):
+                    LOGGER.error(f"No introspection parameters defined! For numeric introspection, insert values for 'acceptable_max' and/or 'acceptable_min'. For non-numerical introspection, insert values for 'contains_str' and/or 'does_not_contain_str'.")
+                    exit()        
 
                 # VALIDATION PROCESS - for numerical values
                 LOGGER.info(f"{40*'-'}\nValue Verification for column '{column_name}':\n{40*'-'}\n")
@@ -167,146 +170,37 @@ class FileIntrospectionPlugin(PluginBase):
                 else:
                     value_to_compare = results_df.loc[value_row, column_name]
 
-                LOGGER.info(f"Value to compare: {value_to_compare}")
+                LOGGER.info(f"Value to compare: {value_to_compare}\n")
                 
                 introspection_success = True
-
-                if (acceptable_min != None or acceptable_max != None): # Numerical introspection
-                    if (value_to_compare <= acceptable_min or value_to_compare >= acceptable_max):
-                        introspection_success = False
-                
-                elif (contains_str != None or does_not_contain_str != None):
-                    pass
                 
                 
+                # Numerical introspection
+                if (acceptable_min != None and value_to_compare <= acceptable_min):
+                    introspection_success = False
+                if (acceptable_max != None and value_to_compare >= acceptable_max):
+                    introspection_success = False
 
+                # Non-numerical introspection
+                if ((contains_str != None or does_not_contain_str != None) and type(value_to_compare) != str):
+                    LOGGER.error(f"Your chosen value to compare is not a string!\n")
+                    exit()
 
-
-
-        ################################
-        # OLD CODE
-        ################################
-
-        # # Check for value abnormalities (optional process)
-        # verification_flag = True
-        
-        # if self.model.columns_to_verify != None:
-
-        #     LOGGER.info(f"{40*'-'}\nValue Verification\n{40*'-'}\n")
-
-        #     column_names = results_df.columns.values.tolist()
-
-        #     for column in self.model.columns_to_verify:
-        #         # Check if the specified column exists in the .csv file
-
-        #         if (column_names.count(column) < 1):
-        #             LOGGER.warning(f"The column specified for verification - {column} -  does not exist in the .csv file. Check for spelling errors.\n")
-        #         else:
-        #             LOGGER.info(f"Verifying column '{column}'\n")
-                    
-        #             # Get max and min values in the column
-        #             column_max = results_df[column].max()
-        #             column_min = results_df[column].min()
-                    
-        #             # RefactoringuUpper and lower limits, if they are specified in Rigelfile
-        #             upper_limit = self.model.column_verification_upper_threshold
-        #             lower_limit = self.model.column_verification_lower_threshold
-
-        #             # Issue a warning if no limit is specified
-        #             if (upper_limit == None and lower_limit == None):
-        #                 LOGGER.warning("No thresholds specified for value verification. Please edit your Rigelfile to include at least one threshold (upper or lower).\n")
-        #             else:
-        #                 pass
-                    
-        #             if (upper_limit != None and column_max >= upper_limit):
-        #                 verification_flag = False
-        #             else:
-        #                 pass
-
-        #             if (lower_limit != None and column_min <= lower_limit):
-        #                 verification_flag = False
-        #             else:
-        #                 pass
-
-
-
-        #     LOGGER.info(f"Are the values verified? {verification_flag}\n")
-
-        #     if not verification_flag:
-        #         LOGGER.error("Abnormal values! Get new values!")
-        #         exit()
-        
-        
-        
-        
-        # # Decide which value to use for comparison according to the Rigelfile
-        # if (self.model.use_latest_row and self.model.use_latest_column):
-            
-        #     value_to_compare = results_df.loc[results_df.index[-1], results_df.columns[-1]]
-
-        #     LOGGER.info(f"{40*'-'}\nValue used for comparison:\n{40*'-'}\n\nRow: {results_df.index[-1]} | Column: {results_df.columns[-1]} | Value: {value_to_compare}\n")
-
-        # else:
-        #     if self.model.use_latest_row:
+                if (contains_str != None and not contains_str in value_to_compare):
+                    introspection_success = False
+                if (does_not_contain_str != None and does_not_contain_str in value_to_compare):
+                    introspection_success = False
                 
-        #         value_to_compare = results_df.loc[results_df.index[-1], self.model.value_column]
 
-        #         LOGGER.info(f"{40*'-'}\nValue used for comparison:\n{40*'-'}\n\nRow: {results_df.index[-1]} | Column: {self.model.value_column} | Value: {value_to_compare}\n")
+                if introspection_success:
+                    LOGGER.info(f"Introspection for column '{column_name}' passed!\n")
+                else:
+                    LOGGER.info(f"Introspection for column '{column_name}' failed!\n")
 
-        #     elif self.model.use_latest_column:
                 
-        #         value_to_compare = results_df.loc[self.model.value_row, results_df.columns[-1]]
-                
-        #         LOGGER.info(f"{40*'-'}\nValue used for comparison:\n{40*'-'}\n\nRow: {self.model.value_row} | Column: {results_df.columns[-1]} | Value: {value_to_compare}\n")
-
-        #     else:
-                
-        #         value_to_compare = results_df.loc[self.model.value_row, self.model.value_column]
-
-        #         LOGGER.info(f"{40*'-'}\nValue used for comparison:\n{40*'-'}\n\nRow:{self.model.value_row} | Column: {self.model.value_column} | Value: {value_to_compare}\n")
-
-
-        # # Quality levels, values defined in Rigelfile
-        # if value_to_compare <= self.model.acceptable_threshold:
-        #     results_quality = "acceptable"
-        # else:
-        #     results_quality = "bad"
-
-        # if value_to_compare <= self.model.good_threshold:
-        #     results_quality = "good"
-
-        # if value_to_compare <= self.model.great_threshold:
-        #     results_quality = "great"
 
 
 
-        # LOGGER.info(f"{40*'-'}\nIntrospection Results:\n{40*'-'}\n\nThe results were {results_quality}!\n")
-
-
-        # # Set "acceptable" as default requirement in case none is passed
-        # if self.model.required_quality_level == None:
-        #     self.model.required_quality_level = "acceptable"
-
-        # #Comparing quality level vs. required quality level    
-        # LOGGER.info(f"Required quality level: {self.model.required_quality_level}\n")
-
-        # if self.model.required_quality_level == "acceptable":
-        #     if results_quality != "bad":
-        #         LOGGER.info("Test passed!\n")
-        #     else:
-        #         LOGGER.info("Test failed!\n")
-
-        # elif self.model.required_quality_level == "good":
-        #     if (results_quality == "good" or results_quality == "great"):
-        #         LOGGER.info("Test passed!\n")
-        #     else:
-        #         LOGGER.info("Test failed!\n")
-
-        # elif self.model.required_quality_level == "great":
-        #     if results_quality == "great":
-        #         LOGGER.info("Test passed!\n")
-        #     else:
-        #         LOGGER.info("Test failed!\n")
 
     def stop(self) -> None:
         # LOGGER.info("Running Stop") # Debug line
